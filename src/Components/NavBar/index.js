96a3e3836cd97b9f3da6c50f1374691173cd2021
SunnyShinde <sunnyshinde1769@gmail.com>
2026-01-02 11:58:57 +0530

    first commit

---
+++ src/Components/NavBar/index.js
@@ -0,0 +1,54 @@
+import React from 'react'
+import { Nav, NavLink, NavbarContainer, Span, NavLogo, NavItems, GitHubButton, ButtonContainer, MobileIcon, MobileMenu, MobileLink } from './NavBarstyledComponents'
+import { DiCssdeck } from 'react-icons/di';
+import { FaBars } from 'react-icons/fa';
+import { Bio } from '../../Data/Constants';
+
+import { useTheme } from 'styled-components';
+
+const Navbar = () => {
+  const [isOpen, setIsOpen] = React.useState(false);
+  const theme = useTheme()
+  return (
+    <Nav>
+      <NavbarContainer>
+      <NavLogo to='/'>
+          <div style={{ display: "flex", alignItems: "center", color: "white", marginBottom: '20px', cursor: 'pointer' }}>
+            <DiCssdeck size="3rem" /> <Span>Portfolio</Span>
+          </div>
+        </NavLogo>
+        <MobileIcon>
+          <FaBars onClick={() => {
+            setIsOpen(!isOpen)
+          }} />
+        </MobileIcon>
+        <NavItems>
+          <NavLink href="#about">About</NavLink>
+          <NavLink href='#skills'>Skills</NavLink>
+          <NavLink href='#experience'>Experience</NavLink>
+        </NavItems>
+        <ButtonContainer>
+          <GitHubButton href={Bio.github} target="_blank">Github Profile</GitHubButton>
+        </ButtonContainer>
+        {
+          isOpen &&
+          <MobileMenu isOpen={isOpen}>
+            <MobileLink href="#about" onClick={() => {
+              setIsOpen(!isOpen)
+            }}>About</MobileLink>
+            <MobileLink href='#skills' onClick={() => {
+              setIsOpen(!isOpen)
+            }}>Skills</MobileLink>
+            <MobileLink href='#experience' onClick={() => {
+              setIsOpen(!isOpen)
+            }}>Experience</MobileLink>
+           
+            <GitHubButton style={{padding: '10px 16px',background: `${theme.primary}`, color: 'white',width: 'max-content'}} href={Bio.github} target="_blank">Github Profile</GitHubButton>
+          </MobileMenu>
+        }
+      </NavbarContainer>
+    </Nav>
+  )
+}
+
+export default Navbar
