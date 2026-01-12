5d2647335e841acf25afb9639e57aa76a53b4cf1
SunnyShinde <sunnyshinde1769@gmail.com>
2026-01-12 20:58:49 +0530

    Resume added

--- src/Data/Constants.js
+++ src/Data/Constants.js
@@ -5,8 +5,7 @@ export const Bio = {
   description:
     "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
   github: "https://github.com/sunnyshinde8097/",
-  resume:
-  ResumePDF,
+  resume:ResumePDF,
   linkedin: "https://www.linkedin.com/in/sunny-shinde-9a342514a/",
 };
 
