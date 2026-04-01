9d347da38c11b840a27e0552cead91e76b6d6ff8
SunnyShinde <sunnyshinde1769@gmail.com>
2026-04-01 16:15:38 +0530

    New Resume

--- src/Components/Skills/index.js
+++ src/Components/Skills/index.js
@@ -128,7 +128,7 @@ const Skills = () => {
     <Container id="skills">
       <Wrapper>
         <Title>Skills</Title>
-        <Desc>Here are some of my skills on which I have been working on for the past 3.3 years.
+        <Desc>Here are some of my skills on which I have been working on for the past 3.5 years.
         </Desc>
         <SkillsContainer>
           {skills.map((skill) => (
